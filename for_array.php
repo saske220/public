f78cb0d7f83797624f896699f41283004b5d66ac
saske220 <146696108+saske220@users.noreply.github.com>
2024-05-09 14:10:35 +0900

    0509

---
+++ for_array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        <?php
+        //p117 6-9
+        $friends = array("はるき","かおる","ひでと");
+        for($i = 0; $i < 3; $i++){
+            echo $friends[$i] . "<br>";
+
+        }
+        ?>
+    </pre>
+
+</body>
+</html>
---
+++ foreach.php
@@ -0,0 +1,11 @@
+<?php
+
+$results = array(
+    "math" => 90,
+    "english" => 80,
+    "japanese" => 85,
+);
+
+foreach($results as $score){
+    echo $score . "<br>";
+}
