c42e525286ace70445d94b6381f21ac155cae844
saske220 <146696108+saske220@users.noreply.github.com>
2024-05-09 14:20:37 +0900

    1

--- foreach.php
+++ foreach.php
@@ -4,8 +4,15 @@ $results = array(
     "math" => 90,
     "english" => 80,
     "japanese" => 85,
+    "history" => 75
 );
 
 foreach($results as $score){
     echo $score . "<br>";
+}
+
+echo "<hr>";
+
+foreach($results as $title => $score){
+    echo $title . ":" . $score . "<br>";
 }
