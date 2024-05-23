765a3cd1adc86ab1d4fba6b3d1bdb9ab270c9ed3
saske220 <146696108+saske220@users.noreply.github.com>
2024-05-23 15:03:05 +0900

    0523

---
+++ tennis/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="ja" >
+  <head>
+    <title>サークルサイト</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php $info = file_get_contents('info.txt'); ?>
+
+    <?php include('navbar.php'); ?>
+
+    <main role="main" class="container" style="padding:60px 15px 0">
+      <div>
+        <!-- ここから「本文」-->
+       <?php $fp =  fopen('info.txt',"r");
+       $title = fgets($fp);?>
+
+        <h1>サークルサイト</h1>
+<?php echo nl2br($info,false); ?>
+
+        <!-- 本文ここまで -->
+      </div>
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
