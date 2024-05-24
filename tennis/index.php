0c40201ba0a2af738417a8b188258a14f228f999
saske220 <146696108+saske220@users.noreply.github.com>
2024-05-24 10:14:09 +0900

    0524

--- tennis/index.php
+++ tennis/index.php
@@ -1,3 +1,13 @@
+<?php
+$info = file_get_contents("info.txt");
+?>
+<!-- 読み込み専用 -->
+<?php
+$fp = fopen("info.txt", "r"); ?>
+<!-- １行だけ読み込む -->
+<?php
+$title = fgets($fp); ?>
+
 <!doctype html>
 <html lang="ja" >
   <head>
@@ -9,19 +19,43 @@
 
     <?php include('navbar.php'); ?>
 
-    <main role="main" class="container" style="padding:60px 15px 0">
-      <div>
-        <!-- ここから「本文」-->
-       <?php $fp =  fopen('info.txt',"r");
-       $title = fgets($fp);?>
+  <main role="main" class="container" style="padding:60px 15px 0">
 
-        <h1>サークルサイト</h1>
-<?php echo nl2br($info,false); ?>
+        <?php include('navbar.php'); ?>
+        <h1>おしらせ</h1>
 
-        <!-- 本文ここまで -->
-      </div>
+        <!-- nl2br:改行 -->
+        <!-- nl2br(文字列 false)→ -->
+        <!-- nl2br(文字列 true)→ -->
+
+        <!-- p161
+        fopenファイルを開く
+        fgetsファイルから１行ずつ読み込む
+        fcloseファイルを閉じる
+
+         p162
+        fopen(ファイル名、モード)モードの詳細はp163
+        読み込み専用で開く場合
+        fopen("info.txt","r")-->
+<div>
+        <?php
+        if($fp){
+            $title = fgets($fp);
+            if($title){
+                echo $title;
+                echo '<p><a href="info.txt">info.txt</a></p>';
+
+            }else{
+                echo '<p>おしらせがありません</p>';
+            }
+        }else{
+            echo "ファイルが開けませんでした";
+        }
+        ?>
+
+        </div>
+
     </main>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
